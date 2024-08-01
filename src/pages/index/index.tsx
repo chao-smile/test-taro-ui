ff0aff22aac76c062d7a7c941c5613f3629eea66
chaosmile <chaosmile404@gmail.com>
2024-08-01 14:36:22 +0800

    feat: 添加测试用例

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,17 +1,47 @@
-import { View, Text } from '@tarojs/components';
+import { View, Image } from '@tarojs/components';
 import { useLoad } from '@tarojs/taro';
-import { AtButton } from 'taro-ui';
+import { useState } from 'react';
+import { AtButton, AtInput } from 'taro-ui';
 import './index.less';
 
 export default function Index() {
+  const [text, setText] = useState<string | number>('');
   useLoad(() => {
     console.log('Page loaded.');
   });
 
   return (
-    <View className="index">
-      <Text>Hello world!</Text>
-      <AtButton type="primary">Button</AtButton>
+    <View className="at-article">
+      <View className="at-article__h1">这是一级标题这是一级标题</View>
+      <View className="at-article__info">2017-05-07&nbsp;&nbsp;&nbsp;这是作者</View>
+      <View className="at-article__content">
+        <View className="at-article__section">
+          <View className="at-article__h2">这是二级标题</View>
+          <View className="at-article__h3">这是三级标题</View>
+          <View className="at-article__p">
+            这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本落。这是文本段落。1234567890123456789012345678901234567890
+            ABCDEFGHIJKLMNOPQRSTUVWXYZ
+          </View>
+          <View className="at-article__p">这是文本段落。这是文本段落。</View>
+          <View className="at-article__p">{text}</View>
+          <Image
+            className="at-article__img"
+            src="https://download.chaosmile.com/unnamed.png"
+            mode="widthFix"
+            style={{ width: '50vw' }}
+          />
+        </View>
+      </View>
+      <AtInput
+        value={text.toString()}
+        onChange={setText}
+        style={{ width: '100vw', height: '50px' }}
+        name="test"
+        placeholder="测试"
+      ></AtInput>
+      <AtButton type="primary" circle>
+        测试
+      </AtButton>
     </View>
   );
 }
