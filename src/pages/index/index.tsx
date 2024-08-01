47888d0f2793f335ee693b4ba662779f98ef1c5a
chaosmile <chaosmile404@gmail.com>
2024-08-01 10:53:45 +0800

    chore: init

---
+++ src/pages/index/index.tsx
@@ -0,0 +1,18 @@
+import { View, Text } from '@tarojs/components'
+import { useLoad } from '@tarojs/taro'
+import { AtButton } from 'taro-ui'
+import './index.less'
+
+export default function Index() {
+
+  useLoad(() => {
+    console.log('Page loaded.')
+  })
+
+  return (
+    <View className='index'>
+      <Text>Hello world!</Text>
+      <AtButton type='primary'>Button</AtButton>
+    </View>
+  )
+}
