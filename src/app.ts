c3044c2a61a958a16f5b72b25e987e8c2480ea46
chaosmile <chaosmile404@gmail.com>
2024-08-01 11:01:20 +0800

    feat: add format

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,15 @@
-import { PropsWithChildren } from 'react'
-import { useLaunch } from '@tarojs/taro'
-import 'taro-ui/dist/style/index.scss' // 引入组件样式
-import './app.less'
+import { PropsWithChildren } from 'react';
+import { useLaunch } from '@tarojs/taro';
+import 'taro-ui/dist/style/index.scss'; // 引入组件样式
+import './app.less';
 
 function App({ children }: PropsWithChildren<any>) {
-
   useLaunch(() => {
-    console.log('App launched.')
-  })
+    console.log('App launched.');
+  });
 
   // children 是将要会渲染的页面
-  return children
+  return children;
 }
 
-export default App
+export default App;
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,18 +1,17 @@
-import { View, Text } from '@tarojs/components'
-import { useLoad } from '@tarojs/taro'
-import { AtButton } from 'taro-ui'
-import './index.less'
+import { View, Text } from '@tarojs/components';
+import { useLoad } from '@tarojs/taro';
+import { AtButton } from 'taro-ui';
+import './index.less';
 
 export default function Index() {
-
   useLoad(() => {
-    console.log('Page loaded.')
-  })
+    console.log('Page loaded.');
+  });
 
   return (
-    <View className='index'>
+    <View className="index">
       <Text>Hello world!</Text>
-      <AtButton type='primary'>Button</AtButton>
+      <AtButton type="primary">Button</AtButton>
     </View>
-  )
+  );
 }
